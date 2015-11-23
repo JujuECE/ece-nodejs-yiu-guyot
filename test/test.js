5a2dd81bee0c8f4b3582083886151582a77bba83
Thierry YIU <Thierry YIU>
2015-11-23 18:35:39 +0100

    modif test.js

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 var should = require('should');
 var users = require ('../users')
 describe('server', function () {
-  it('start & stop', function () {
+  it('Test user', function () {
   users.get(11, function (user) {
 	user.id.should.equal(11);
   })
