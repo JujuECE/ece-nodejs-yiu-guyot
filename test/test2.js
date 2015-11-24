72efe805013ab4a59369c21d78f9cd0fda4b2553
jules guyot <jules guyot>
2015-11-24 11:21:56 +0100

    correction fichiers test

--- test/test2.js
+++ test/test2.js
@@ -1,9 +1,9 @@
 var should = require('should');
 var users = require ('../lib/users');
-describe('Test1 / pass', function () {
-  it('Test1 user', function () {
+describe('Test2 / fail', function () {
+  it('Test2 user', function () {
   users.get(11, function (user) {
-	user.should.be.equal(11);
+	user.should.be.equal(12);
   })
   });
 });
